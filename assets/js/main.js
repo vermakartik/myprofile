023a05b23d2b8e5cd67c91e0ffe2b8fc466b5036
Kartik Verma <kvs1297@gmail.com>
2019-08-03 15:31:00 +0530

    added condition for container to become visible only for larger screens and not for smallers screens because on smaller screens it reduces the space available for showing the content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 window.onscroll = () => stickyNav()
-// window.onLoad = () => stickyNav()
+window.onload = () =>  {
+    stickyNav();
+    adjustDisplayHeight();
+}
 
 let navbar = document.querySelector('#normalnav')
 let navbar_offset = navbar.offsetHeight
 let sticky_navbar = document.querySelector('#stickynav')
+let main_tag = document.querySelector('main')
+console.log(main_tag)
 
 let stickyNav = () => {
 
@@ -14,5 +19,13 @@ let stickyNav = () => {
         sticky_navbar.classList.remove('sticky-nav-visible')
         navbar.classList.remove('nav-not-visible')
 
+    }
+}
+
+let adjustDisplayHeight = () => {
+    if(window.innerWidth <= 768){
+        main_tag.classList.remove('container')
+    } else {
+        main_tag.classList.add('container')
     }
 }
